3114ca0dcf320502a77f55ea570f6573d6c8e07c
shadowindustries09 <shadowindustries09@gmail.com>
2026-07-30 11:55:44 +0100

    Remove sitemap integration from astro.config.mjs
    Removed sitemap integration from Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://shadowtechlife.com',
-  integrations: [sitemap()],
 });
+
